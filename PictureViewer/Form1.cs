1131b752c5dc32d5e936f94ffba5bdd9d7761a03
klayza <55328479+klayza@users.noreply.github.com>
2021-05-13 12:04:00 -0700

    Add files via upload

---
+++ PictureViewer/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangeTheme(ColorScheme scheme, Control.ControlCollection container)
+        {
+            foreach (Control component in container)
+            {
+                if (component is Panel)
+                {
+                    ChangeTheme(scheme, component.Controls);
+                    component.BackColor = scheme.PanelBG;
+                    component.ForeColor = scheme.PanelFG;
+                }
+                else if (component is Button)
+                {
+                    component.BackColor = scheme.ButtonBG;
+                    component.ForeColor = scheme.ButtonFG;
+                }
+                else if (component is TextBox)
+                {
+                    component.BackColor = scheme.TextBoxBG;
+                    component.ForeColor = scheme.TextBoxFG;
+                }
+            }
+    
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Show the color dialog box. If the user clicks OK, change the
+            // PictureBox control's background to the color the user chose.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Load(openFileDialog1.FileName);
+                }
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the picture.
+            pictureBox1.Image = null;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // If the user selects the Stretch check box, 
+            // change the PictureBox's
+            // SizeMode property to "Stretch". If the user clears 
+            // the check box, change it to "Normal".
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void openfileButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+    }
+}
